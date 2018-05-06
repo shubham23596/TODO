2f90a0e3981f534e49ee1c41a5fdcf997e3a431e
shubham23596 <shubham23596@gmail.com>
2018-05-06 15:12:13 +0530

    convert to mvc

--- TODO/Controllers/ToDoListViewController.swift
+++ TODO/Controllers/ToDoListViewController.swift
@@ -9,11 +9,14 @@
 import UIKit
 
 class ToDoListViewController: UITableViewController {
-var itemArry = ["click add button to add tasks"]
+var itemArry = [item]()
     let userDefault = UserDefaults.standard
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let items = userDefault.array(forKey: "ToDoListArray") as? [String]{
+        let  newItem = item()
+        newItem.title = "Let's add your tasks"
+        itemArry.append(newItem)
+        if let items = userDefault.array(forKey: "ToDoListArray") as? [item]{
             itemArry = items
         }
         // Do any additional setup after loading the view, typically from a nib.
@@ -23,25 +26,26 @@ var itemArry = ["click add button to add tasks"]
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell")
-        cell?.textLabel?.text = itemArry[indexPath.row]
+        let item = itemArry[indexPath.row]
+        cell?.textLabel?.text = item.title
+        cell?.accessoryType = item.done == true ? .checkmark : .none
         return cell!
     }
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }
-        else{
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        itemArry[indexPath.row].done = !itemArry[indexPath.row].done
+    tableView.reloadData()
         tableView.deselectRow(at: indexPath, animated: true)
     }
     // Mark: Add new items
     @IBAction func addNewItems(_ sender: UIBarButtonItem) {
-        var textField = UITextField()
+        let newIem = item()
+         var textField = UITextField()
+        newIem.title = textField.text!
+       self.itemArry.append(newIem)
         let alert = UIAlertController(title: "Add New Task", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Item", style: .default){(action) in
             //print(textField.text)
-            self.itemArry.append(textField.text!)
+          
             self.userDefault.set(self.itemArry, forKey: "ToDoListArray")
             self.tableView.reloadData()
         }
---
+++ TODO/DataModel/item.swift
@@ -0,0 +1,13 @@
+//
+//  item.swift
+//  TODO
+//
+//  Created by shubham jain on 06/05/18.
+//  Copyright © 2018 shubham jain. All rights reserved.
+//
+
+import Foundation
+class item{
+    var title:String = ""
+    var done:Bool = false
+}
