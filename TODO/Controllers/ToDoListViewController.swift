c49d5a9b4d9d4e0ab631dd7d0bc4c4652a3c7194
shubham23596 <shubham23596@gmail.com>
2018-05-08 09:33:13 +0530

    file storage with plist

--- TODO/Controllers/ToDoListViewController.swift
+++ TODO/Controllers/ToDoListViewController.swift
@@ -10,15 +10,13 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
 var itemArry = [item]()
-    let userDefault = UserDefaults.standard
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
     override func viewDidLoad() {
         super.viewDidLoad()
         let  newItem = item()
         newItem.title = "Let's add your tasks"
         itemArry.append(newItem)
-        if let items = userDefault.array(forKey: "ToDoListArray") as? [item]{
-            itemArry = items
-        }
+      LoadItems()
         // Do any additional setup after loading the view, typically from a nib.
     }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -33,21 +31,22 @@ var itemArry = [item]()
     }
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         itemArry[indexPath.row].done = !itemArry[indexPath.row].done
-    tableView.reloadData()
+        self.saveItems()
+
         tableView.deselectRow(at: indexPath, animated: true)
     }
     // Mark: Add new items
     @IBAction func addNewItems(_ sender: UIBarButtonItem) {
-        let newIem = item()
-         var textField = UITextField()
-        newIem.title = textField.text!
-       self.itemArry.append(newIem)
+        
+ 
+           var textField = UITextField()
         let alert = UIAlertController(title: "Add New Task", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Item", style: .default){(action) in
             //print(textField.text)
-          
-            self.userDefault.set(self.itemArry, forKey: "ToDoListArray")
-            self.tableView.reloadData()
+        let newIem = item()
+            newIem.title = textField.text!
+            self.itemArry.append(newIem)
+            self.saveItems()
         }
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Create new Task"
@@ -57,6 +56,26 @@ var itemArry = [item]()
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
     }
-    
+    func saveItems(){
+        let encoder = PropertyListEncoder()
+        do {
+            let data = try encoder.encode(itemArry)
+            try data.write(to: dataFilePath!)
+        }
+        catch{
+            print("error encoding item array, \(error)")
+        }
+        self.tableView.reloadData()
+    }
+    func LoadItems(){
+        if let data = try? Data(contentsOf: dataFilePath!){
+            let decoder = PropertyListDecoder()
+            do {
+                itemArry = try decoder.decode([item].self, from: data)
+            } catch{
+                print("error\(error)")
+            }
+        }
+    }
 }
 
--- TODO/DataModel/item.swift
+++ TODO/DataModel/item.swift
@@ -7,7 +7,7 @@
 //
 
 import Foundation
-class item{
-    var title:String = ""
-    var done:Bool = false
+class item: Codable{
+    var title: String = ""
+    var done: Bool = false
 }
